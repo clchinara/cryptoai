47d398cd1cd6496b163b99103f1b5cb283c96f8f
clchinara <audrey.chinara@gmail.com>
2023-10-15 03:47:39 +0800

    Validate request body in endpoints

--- server/app.py
+++ server/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
+from validators import validate_speck_request, validate_demo_request, validate_eval_request
+from app_utils import construct_error_response, construct_success_response
 import utils
 import inv1_eval, inv3_eval
 
@@ -22,23 +24,30 @@ def hello_world():
 @app.route('/speck', methods=['POST'])
 def speck():
     data = request.get_json()
-    numPlaintexts = data['numPlaintexts']
+    is_valid, err_msg = validate_speck_request(data)
+    if not is_valid:
+        return jsonify(construct_error_response(msg=err_msg)), 400
+    num_plaintexts = data['numPlaintexts']
     nr = data['numRounds']
     plaintexts = []
     for p in data['plaintexts']:
         plaintexts.append(utils.parse_hex_string(p))
     ciphertexts = None
-    if numPlaintexts == 4:
-        ciphertexts = inv1_eval.speck_func(nr, plaintexts)
-    elif numPlaintexts == 8:
-        ciphertexts = inv1_eval.speck_func(nr, plaintexts)
+    try:
+        if num_plaintexts == 4:
+            ciphertexts = inv1_eval.speck_func(nr, plaintexts)
+        elif num_plaintexts == 8:
+            ciphertexts = inv1_eval.speck_func(nr, plaintexts)
+    except Exception as e:
+        print(f'Error occurred: {e}')
+        return jsonify(construct_error_response(msg='Internal error')), 500
     for i in range(len(ciphertexts)):
         ciphertexts[i] = hex(ciphertexts[i])[2:]
-    res = {
+    res_data = {
         'ciphertexts': ciphertexts
     }
-    print('res:', res)
-    return jsonify(res)
+    print('res_data:', res_data)
+    return jsonify(construct_success_response(data=res_data)), 200
 
 """
 {
@@ -50,33 +59,47 @@ def speck():
 @app.route('/demo', methods=['POST'])
 def demo():
     data = request.get_json()
-    numCiphertexts = data['numCiphertexts']
+    is_valid, err_msg = validate_demo_request(data)
+    if not is_valid:
+        return jsonify(construct_error_response(msg=err_msg)), 400
+    num_ciphertexts = data['numCiphertexts']
     nr = data['numRounds']
     ciphertexts = []
     for c in data['ciphertexts']:
         ciphertexts.append(utils.parse_hex_string(c))
     prob = None
     pred = None
-    if numCiphertexts == 4:
-        prob, pred = inv1_eval.demo_func(nr, ciphertexts)
-    elif numCiphertexts == 8:
-        prob, pred = inv3_eval.demo_func(nr, ciphertexts)
-    res = {
-        'numCiphertexts': numCiphertexts,
+    try:
+        if num_ciphertexts == 4:
+            prob, pred = inv1_eval.demo_func(nr, ciphertexts)
+        elif num_ciphertexts == 8:
+            prob, pred = inv3_eval.demo_func(nr, ciphertexts)
+    except Exception as e:
+        print(f'Error occurred: {e}')
+        return jsonify(construct_error_response(msg='Internal error')), 500
+    res_data = {
+        'numCiphertexts': num_ciphertexts,
         'prob': str(prob),
         'pred': bool(pred)
     }
-    print('res:', res)
-    return jsonify(res)
+    print('res_data:', res_data)
+    return jsonify(construct_success_response(data=res_data)), 200
 
 @app.route('/eval', methods=['POST'])
 def eval():
     data = request.get_json()
+    is_valid, err_msg = validate_eval_request(data)
+    if not is_valid:
+        return jsonify(construct_error_response(msg=err_msg)), 400
     inv = data['inv']
     eval_result = None
-    if inv == 'inv1':
-        eval_result = inv1_eval.eval_func()
-    elif inv == 'inv3':
-        eval_result = inv3_eval.eval_func()
+    try:
+        if inv == 'inv1':
+            eval_result = inv1_eval.eval_func()
+        elif inv == 'inv3':
+            eval_result = inv3_eval.eval_func()
+    except Exception as e:
+        print(f'Error occurred: {e}')
+        return jsonify(construct_error_response(msg='Internal error')), 500
     print('eval_result:', eval_result)
-    return jsonify(eval_result)
+    return jsonify(construct_success_response(data=eval_result)), 200
